4ad6f8dbe4df0403c2bcda97f6fe2a56c1dd4436
Stephen Lizcano <stephen@aesuna.io>
2020-02-15 12:21:16 +0100

    stargazer cloud

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { hostname } = require("os");
 
 const http = require("http");
 
-const message = "Hello World from Stargazer!!\n";
+const message = "Hello World from Stargazer Cloud!!!!\n";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
