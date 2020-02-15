a71ab4dc8d0105b729c755fae0c69d0e02351c48
Stephen Lizcano <stephen@aesuna.io>
2020-02-15 11:28:51 +0100

    fix issues

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,33 +3,30 @@
 STACK_NAME=awsbootstrap
 REGION=eu-central-1
 CLI_PROFILE=awsbootstrap
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-  --query "Account" --output text`
 EC2_INSTANCE_TYPE=t2.micro
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+        --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 # Generate a personal access token with repo and admin:repo_hook
-# permissions form github tokens
-
-GH_ACCESS_TOKEN=$(cat ~/.githib/aws-bootstrap-access-token)
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
-
-# Deploy static resources
-echo -e "\n\n==Deploying setup.yml =="
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME-setup \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
-  --capabilties CAPABILITY_NAMED_IAM \
+  --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET \
-    GitHubOwner=$GH_OWNER \
-    GitHubRepo=$GH_REPO \ GitHubBranch=$GH_BRANCH \ GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \ CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -42,6 +39,11 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
